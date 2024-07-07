22ea5e2d1ac291f59273121fa90b8194b44057e6
ChanHyuc <whk1140@gmail.com>
2024-07-07 21:57:04 +0900

    Feat: InputMenu 생성 및 while문을 통한 loop 실행

--- NumberBaseball/main.swift
+++ NumberBaseball/main.swift
@@ -1,1 +1,17 @@
+private var life = 10
+private var inputMenu = ""
 
+private func gameMenu() {
+    while inputMenu != "2" {
+        print("1. 게임 시작")
+        print("2. 게임 종료")
+        print("원하는 기능을 선택해 주세요", terminator: " : ")
+        
+        guard let input = readLine() else {
+            return
+        }
+        inputMenu = input
+    }
+}
+
+gameMenu()
